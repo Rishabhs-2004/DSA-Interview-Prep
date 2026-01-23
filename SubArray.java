2b3b81a787f493d138bd57985b2609e23091a85d
Rishabh Singh <rishabhsingh9501@gmail.com>
2026-01-23 09:15:11 -0800

    update

--- SubArray.java
+++ SubArray.java
@@ -5,9 +5,9 @@ public class SubArray {
          int totalSum = 0;
          for (int i = 0; i < arr.length; i++) {
             int start = i;
-            for (int j = i+1; j < arr.length; j++) {
+            for (int j = i; j<arr.length; j++) {
                 int end = j;
-                for (int k = i; k <= j; k++) {
+                for (int k = start; k<=end; k++) {
                     System.out.print(arr[k] + " ");
                 }
                 totalSum++;
