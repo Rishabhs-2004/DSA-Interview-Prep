c840ab18377e732309f9099352aa2a24fd5ecebd
Rishabh Singh <rishabhsingh9501@gmail.com>
2026-04-06 09:50:03 -0700

    6/Aprial/2026

---
+++ Relational.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Relational {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+             
+        
+        int a= 10;
+        int b= 20;
+
+        System.out.println(a==b); // equai operator.
+        System.out.println(a>b); // greater operator.
+        System.out.println(a>=b); // greater than equal operator.
+        System.out.println(a<b); // less operator.
+        System.out.println(a<=b); // less than equal operator.
+        System.out.println(a!=b); //  not equai operator.
+
+    }
+}
---
+++ Variable.java
@@ -0,0 +1,10 @@
+public class Variable {
+    public static void main(String[] args) {
+        
+       int a = 20;
+        int b = 50;
+         String name = "Rishabh";
+         System.out.print(name);
+        System.out.print(a+b);
+    }
+}
