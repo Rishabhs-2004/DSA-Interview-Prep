10c6bf5a2eb5a2c719fd443169f84ce21961f9ac
Rishabh Singh <rishabhsingh9501@gmail.com>
2026-04-12 00:54:01 -0700

    update Aprial

---
+++ If_Else.java
@@ -0,0 +1,12 @@
+public class If_Else {
+    public static void main(String[] args) {
+        
+     int age = 24;
+
+       if(age >= 18){
+        System.out.println("you can voit");
+       }else{
+        System.out.println("you cannot vote");
+       }
+    }
+}
---
+++ Largest_of_two_num.java
@@ -0,0 +1,19 @@
+public class Largest_of_two_num {
+    public static void main(String[] args) {
+         
+
+        int a= 120;
+        int b= 40;
+
+
+        
+        if(a>=b){
+            System.out.println("A is the largest value");
+        }if(a>=b && b>=a){
+            System.out.println("some value is equal");
+        }
+        else{
+            System.out.println("b is the lowest value");
+        }
+    }
+}
