09a5db84515135b5b62b0eecf0c9987b6350ae7f
Rishabh Singh <rishabhsingh9501@gmail.com>
2026-04-25 17:03:07 -0700

    update

--- StarPrint.java
+++ StarPrint.java
@@ -1,8 +1,10 @@
 public class StarPrint {
  public static void main(String[] args) {
-    System.out.println("****");
-    System.out.println("***");
-    System.out.println("**");
-    System.out.println("*");
+       for(int i = 1; i<=4; i++){
+                 for(int j=1; j<=i; j++){
+                  System.out.print("*");
+                 }
+                 System.out.println();
+       }
  }   
 }
